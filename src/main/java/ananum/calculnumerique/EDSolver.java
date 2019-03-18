a1769dc67a8c5bf5e2dcd6323befa26ffd3ee7e7
RMPR <akoudanilo@gmail.com>
2019-03-18 12:38:33 +0100

    Initialization of repository

---
+++ src/main/java/ananum/calculnumerique/EDSolver.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ananum.calculnumerique;
+
+/**
+ *
+ * @author Rufus
+ */
+public class EDSolver {
+	@Override
+	public Function solve(Function f, int n, double a, double b) {
+		Double[] values = f.f(n);
+
+		double pas = values[1] - values[0];
+		return null;
+	}	
+}
---
+++ src/main/java/ananum/calculnumerique/Solver.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ananum.calculnumerique;
+
+/**
+ *
+ * @author Rufus
+ */
+public interface Solver {
+    public Function solve(Function f, int n, double a, double b);	
+}
