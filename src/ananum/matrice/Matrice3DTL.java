5f51ccbbe916543b70b45131344553e4daab6809
jodel <fokoujodel@yahoo.fr>
2019-04-29 08:41:25 +0100

    Jodel Push

--- src/ananum/matrice/Matrice3DTL.java
+++ src/ananum/matrice/Matrice3DTL.java
@@ -1,5 +1,7 @@
 package ananum.matrice;
 
+import java.util.HashMap;
+
 /*
  *Matrice de Toeplitz
  *Matrice3DTL: permet de stocker les matrices tridiagonal qui possede les memes elements sur toute la diagonal
@@ -7,6 +9,7 @@ package ananum.matrice;
 public class Matrice3DTL extends Matrice {
 
     private int n;
+    private HashMap<Integer, Double> registre = new HashMap<Integer, Double>();
     /*permet de stocker les elements importants
     indice 0: pour la diagonal inferieur
     indice 1: pour la grande diagonal
@@ -16,7 +19,7 @@ public class Matrice3DTL extends Matrice {
 
     public Matrice3DTL(int n) {
         if (n <= 0) {
-            n = -1;
+            this.n = -1;
             tab = null;
         } else {
             this.n = n;
@@ -49,23 +52,21 @@ public class Matrice3DTL extends Matrice {
         }
     }
 
+    private Integer hash(int i, int j) {
+        return i + n * j;
+    }
+
     @Override
     public void set(int i, int j, double x) {
         if (mauvaixIndex(i, j) || this.n <= 0) return;
-        if (n > 1) {
-            int ind = j - i + 1;
-            if (ind < 0 || ind > 2) return;
-            tab[ind] = x;
-        } else {
-            int ind = j - i;
-            if (ind != 0) return;
-            tab[ind] = x;
-        }
+        registre.put(hash(i, j), x);
     }
 
     @Override
     public double get(int i, int j) {
         if (mauvaixIndex(i, j) || this.n <= 0) return 0.0;
+        Double reg = registre.get(hash(i, j));
+        if (reg != null) return reg;
         if (n > 1) {
             int ind = j - i + 1;
             if (ind < 0 || ind > 2) return 0;
